a48104b6f19bb4038838beb6fd1853149cb98888
imkdw <imkdw@kakao.com>
2023-07-02 00:09:02 +0900

    Update 10798.py

--- BOJ/단계별로 풀어보기/10798.py
+++ BOJ/단계별로 풀어보기/10798.py
@@ -1,20 +1,6 @@
-all_words = []
-max_len = 0
-
-for i in range(5):
-    all_words.append(list(input()))
-
-
-
-for words in all_words:
-    if max_len < len(words):
-        max_len = len(words)
-
-
-
-print(max_len)
+all_words = [list(input()) for i in range(5)]
+max_len = max(len(i) for i in all_words)
+with_space_words = [arr + [''] * (max_len - len(arr)) for arr in all_words]
 for i in range(max_len):
     for j in range(5):
-        print(all_words[j][i], end="")
-
-
+        print(with_space_words[j][i], end="")
