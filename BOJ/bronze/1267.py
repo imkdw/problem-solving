04c1e83b58c7afc0030b08a98ef7cff83f531cbb
imkdw <imkdw@kakao.com>
2023-07-12 22:11:10 +0900

    Update 1267.py

--- BOJ/bronze/1267.py
+++ BOJ/bronze/1267.py
@@ -1,8 +1,14 @@
 N = int(input())
 call_time = map(int, input().split())
+
+y_price, m_price = 0, 0
 for i in call_time:
-    y_price = i // 30 * 10 + 10
-    m_price = i // 60 * 15 + 15
-    print(y_price, m_price)
+    y_price += (i // 30 + 1) * 10
+    m_price += (i // 60 + 1) * 15
 
-
+if y_price > m_price:
+    print("M", m_price)
+elif y_price < m_price:
+    print("Y", y_price)
+elif y_price == m_price:
+    print("Y M", m_price)
